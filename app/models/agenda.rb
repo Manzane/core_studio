9ce175d96a8efe61e06d4f6a4c2717a24f65680e
marie <marie.blanchet.pro@gmail.com>
2020-07-18 12:14:33 +0200

    models validations and associationss

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,3 +1,9 @@
 class Agenda < ApplicationRecord
+  has_many :bookings
   belongs_to :lesson
+
+  validates :time, :capacity, presence: true
+
+  def full?
+  end
 end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,3 +1,6 @@
 class Package < ApplicationRecord
   belongs_to :category
+  has_many :cart_items, dependent: :destroy
+  
+  validates :quantity, :price, presence: true
 end
