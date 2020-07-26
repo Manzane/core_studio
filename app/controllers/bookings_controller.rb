37fd1f8aaa41a40adfce8876be4ea6e95f393c5c
marie <marie.blanchet.pro@gmail.com>
2020-07-26 14:55:40 +0200

    increase credits while booking cancellations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  before_action :set_lesson, only: [:new, :create]
   def index
     @bookings = current_user.bookings.all
   end
@@ -9,7 +10,6 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
-    @lesson = Lesson.find(params[:lesson_id])
     credits = current_user.credits.find_by(category_id: @lesson.category_id)
     if credits
       @credits = credits.quantity
@@ -22,7 +22,6 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    @lesson = Lesson.find(params[:lesson_id])
     @booking.lesson_id = @lesson.id
     @booking.user_id = current_user.id
     credits_updater = CreditsUpdater.new(booking_params, current_user, @lesson)
@@ -46,6 +45,9 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @lesson = @booking.lesson
+    credits_updater = CreditsUpdater.new(@booking, current_user, @lesson)
+    credits_updater.increase
     @booking.destroy
     redirect_to bookings_path, notice: "Réservation annulée"
   end
@@ -55,6 +57,10 @@ class BookingsController < ApplicationController
 
   def set_booking
     @booking = current_user.bookings.find(params[:id])
+  end
+
+  def set_lesson
+    @lesson = Lesson.find(params[:lesson_id])
   end
 
   def booking_params
--- app/services/credits_updater.rb
+++ app/services/credits_updater.rb
@@ -6,7 +6,15 @@ class CreditsUpdater
     end
     attr_accessor :credits
     def increase
-      
+        category = Category.find(@lesson.category_id)
+        credit = @user.credits.find_by(category_id: category.id)
+        # raise
+        credit.quantity += @params[:quantity].to_i
+        if credit.save!
+            return true
+        else
+            return false
+        end
     end
     def check_credits
         category = Category.find(@lesson.category_id)
