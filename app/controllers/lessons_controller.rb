2f28d5cdcfc9d0a846b236754982d8c7d8ad56cd
marie <marie.blanchet.pro@gmail.com>
2020-08-27 16:41:56 +0200

    fix current_user and credits while nil in the planning view

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,7 +4,11 @@ class LessonsController < ApplicationController
     def index
         @lessons = Lesson.all
         @booking = Booking.new
-        @credits = current_user.credits
+        if current_user
+            if current_user.credits
+                @credits = current_user.credits
+            end
+        end
         # @calendar_events = @lessons.flat_map{ |e| e.calendar_events(params.fetch(:start_date, Time.zone.now).to_date) }
     end
 
