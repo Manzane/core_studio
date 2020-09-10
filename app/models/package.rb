29f1a86ed1bbfbc168e3719a02e74ca3d8558349
marie <marie.blanchet.pro@gmail.com>
2020-09-10 19:05:09 +0200

    active storage and image upload

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,6 +2,7 @@ class Package < ApplicationRecord
   belongs_to :category
   has_many :cart_items, dependent: :destroy
   monetize :price_cents
+  has_one_attached :image
   
   validates :quantity, presence: true
 end
