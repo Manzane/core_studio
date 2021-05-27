41fb72f53985ebfecdde77705dd500c1ad2f13dd
marie blanchet <marie.blanchet.pro@gmail.com>
2021-05-27 11:01:30 +0200

    update sql payment query and update gemfile

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsController < ApplicationController
     def new
-        @order = current_user.orders.where(state: 'en cours').find(params[:order_id])
+        # @order = current_user.orders.where(state: 'en cours').find(params[:order_id])
+        @order = current_user.orders.where("state LIKE ?", "en cours").find(params[:order_id])
       end
 end
