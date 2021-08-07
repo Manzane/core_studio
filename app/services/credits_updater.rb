ad2bd785a8f7e2fc0616a22c7ed5ec49d65898a4
marie blanchet <marie.blanchet.pro@gmail.com>
2021-08-07 12:45:17 +0200

    user can booking only one place for a lesson

--- app/services/credits_updater.rb
+++ app/services/credits_updater.rb
@@ -4,6 +4,7 @@ class CreditsUpdater
     def initialize(params, user, options={})
       @params = params
       @user = user
+      @quantity = 1
       if options.has_key?(:lesson)
         @lesson = options[:lesson]
       end
@@ -12,7 +13,8 @@ class CreditsUpdater
     def check_credits
         category = Category.find(@lesson.category_id)
         credit = @user.credits.find_by(category_id: category.id)
-        if credit.quantity >= @params[:quantity].to_i
+        # if credit.quantity >= @params[:quantity].to_i
+        if credit.quantity >= @quantity
             return true
         else
             return false
@@ -23,7 +25,8 @@ class CreditsUpdater
         category = Category.find(@lesson.category_id)
         credit = @user.credits.find_by(category_id: category.id)
         # raise
-        credit.quantity += @params[:quantity].to_i
+        # credit.quantity += @params[:quantity].to_i
+        credit.quantity += @quantity
         if credit.save!
             return true
         else
@@ -38,7 +41,8 @@ class CreditsUpdater
             category = Category.find(@params["category"].to_i)
         end
         credit = @user.credits.find_by(category_id: category.id)
-        credit.quantity -= @params["quantity"].to_i
+        # credit.quantity -= @params["quantity"].to_i
+        credit.quantity -= @quantity
         # raise
         if credit.save!
             return true
@@ -49,22 +53,23 @@ class CreditsUpdater
 
     def order
         # raise
-            # binding.pry
-            category = Category.find(@params["category"].to_i)
-            quantity = @params["quantity"].to_i
-            # raise
-            # binding.pry
-            if @user.credits.find_by(category: category)
-                credit = @user.credits.find_by(category_id: category.id)
-                credit.quantity += quantity
-                if !credit.save!
-                    return false
-                end
-            else
-                credit = Credit.create!(user: @user, category: category, quantity: quantity)
-                if !credit.save!
-                    return false
-                end
+        # binding.pry
+        category = Category.find(@params["category"].to_i)
+        # quantity = @params["quantity"].to_i
+        quantity = @quantity
+        # raise
+        # binding.pry
+        if @user.credits.find_by(category: category)
+            credit = @user.credits.find_by(category_id: category.id)
+            credit.quantity += quantity
+            if !credit.save!
+                return false
             end
+        else
+            credit = Credit.create!(user: @user, category: category, quantity: quantity)
+            if !credit.save!
+                return false
+            end
+        end
     end 
   end
